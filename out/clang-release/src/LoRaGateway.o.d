0eafb357fc4636fe5ff95b1faa18f991f31be0b1
kroja-nk <rizkyaqil04@gmail.com>
2024-11-19 14:16:20 +0700

    miaw

--- out/clang-release/src/LoRaGateway.o.d
+++ out/clang-release/src/LoRaGateway.o.d
@@ -1,4 +1,3 @@
 ../out/clang-release/src/LoRaGateway.o: LoRaGateway.cc \
-  E:/Omnet/omnetpp-6.1/include/omnetpp.h
-
-E:/Omnet/omnetpp-6.1/include/omnetpp.h:
+  /home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h
+/home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h:
--- out/clang-release/src/LoRaSensorNode.o.d
+++ out/clang-release/src/LoRaSensorNode.o.d
@@ -1,4 +1,3 @@
 ../out/clang-release/src/LoRaSensorNode.o: LoRaSensorNode.cc \
-  E:/Omnet/omnetpp-6.1/include/omnetpp.h
-
-E:/Omnet/omnetpp-6.1/include/omnetpp.h:
+  /home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h
+/home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h:
