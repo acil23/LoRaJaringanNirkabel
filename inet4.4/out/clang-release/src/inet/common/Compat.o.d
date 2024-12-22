9ea87fb95d66a9b366544d879ed85c63011e5c7b
kroja-nk <rizkyaqil04@gmail.com>
2024-12-22 10:55:08 +0700

    gtaulagi dah

---
+++ inet4.4/out/clang-release/src/inet/common/Compat.o.d
@@ -0,0 +1,5 @@
+../out/clang-release/src/inet/common/Compat.o: inet/common/Compat.cc \
+  inet/common/Compat.h \
+  /home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h
+inet/common/Compat.h:
+/home/foxcat/Downloads/omnetpp-6.1/include/omnetpp.h:
